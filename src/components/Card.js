ad268e8de0094635e51d4c7cf0e74fb6270235f1
Anna Zhukova <anna.zhukanna@gmail.com>
2022-03-19 01:18:02 +0500

    refactoring card and book page styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Card.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Card.css";
 
-function Card({ id, img, title, authors, category}) {
+function Card({ id, img, title, authors, category }) {
+  if (!img) img = `https://picsum.photos/seed/${id}/180/240?grayscale&blur=8`;
+  if (category) category = category.slice(0, 1);
 
-    if (!img) img=`https://picsum.photos/seed/${id}/180/240?grayscale&blur=8`;
-    if (category) category = category.slice(0,1);
-
-    return (
-        <Link to={`/Book/${id}`} className="card card--link" data-id={id}>
-            <img src={img} className="card-img-top card_img" alt={title}></img>
-            <div className="card-body">
-                <h5 className="card_title">{title}</h5>
-                <h6 className="card_subtitle">{category}</h6>
-                <p className="card_text">{authors?.join(', ')}</p>
-            </div>
-        </Link>
-    )
+  return (
+    <Link to={`/Book/${id}`} className="card card--link" data-id={id}>
+      <img src={img} className="card-img-top card_img" alt={title}></img>
+      <div className="card-body">
+        <h5 className="card_title">{title}</h5>
+        <h6 className="card_subtitle">{category}</h6>
+        <p className="card_text">{authors?.join(", ")}</p>
+      </div>
+    </Link>
+  );
 }
 
 export default Card;
--- src/pages/book.js
+++ src/pages/book.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import { React, useEffect } from "react";
+import '../components/Card.css';
 
 function Book() {
-    return <>
-            <h2>Some book</h2>
+  /*     useEffect(() => {
+        document.title = title;
+      }); */
 
-            <div className="card mb-3">
+  return (
+      <div className="card" style={{ width: "100%", flexDirection: "row" }}>
         <div className="row g-0">
-            <div className="col-md-4">
-            <img src="https://picsum.photos/300/400" className="img-fluid rounded-start" alt="..."></img>
-            </div>
-            <div className="col-md-8">
+          <div className="col-md-4">
+            <img
+              src="https://picsum.photos/300/400"
+              className="img-fluid rounded-start card_img"
+              alt="..."
+            ></img>
+          </div>
+          <div className="col-md-8">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              <h2 className="card_title">Book title</h2>
+              <h6 className="card_subtitle">Category</h6>
+              <p className="card-text">
+                <small>
+                  Author 1, Author 2, Author 3, Author 4
+                </small>
+              </p>
+              <p className="card_text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer. This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
-        </>
+      </div>
+  );
 }
 
 export default Book;
