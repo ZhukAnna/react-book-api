0f937c8b86e59482876e49018a72b402de8eb18e
Anna Zhukova <anna.zhukanna@gmail.com>
2022-03-25 14:17:50 +0500

    add handlers on filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import './Filter.css';
+import React, { useState } from "react";
+import "./Filter.css";
 
 function Filter() {
-    return (
-        <div className='filter'>
-            <div>
-                <label for="category" className="form-label filter_label">Category</label>
-                <select className="filter_select" ariaLabel="category" id="category">
-                <option>all</option>
-                <option value="art">art</option>
-                <option value="biography">biography</option>
-                <option value="computers">computers</option>
-                <option value="history">history</option>
-                <option value="medical">medical</option>
-                <option value="poetry">poetry</option>
-                </select>
-            </div>
-            <div>
-                <label for="sort" className="form-label filter_label">Sort by</label>
-                <select className="filter_select" ariaLabel="sort" id="sort">
-                <option  value="relevance">relevance</option>
-                <option value="newest">newest</option>
-                </select>
-            </div>
-        </div>      
-        )
+  const [category, setCategory] = useState({ value: "all" });
+  const handleChangeCategory = (event) => {
+    setCategory({ value: event.target.value });
+  };
+
+  const [sorting, setSorting] = useState({ value: "all" });
+  const handleChangeSorting = (event) => {
+    setSorting({ value: event.target.value });
+  };
+
+  return (
+    <div className="filter">
+      <div>
+        <label htmlFor="category" className="form-label filter_label">
+          Category
+        </label>
+        <select
+          className="filter_select"
+          aria-label="category"
+          id="category"
+          value={category.value}
+          onChange={handleChangeCategory}
+        >
+          <option value="all">all</option>
+          <option value="art">art</option>
+          <option value="biography">biography</option>
+          <option value="computers">computers</option>
+          <option value="history">history</option>
+          <option value="medical">medical</option>
+          <option value="poetry">poetry</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="sort" className="form-label filter_label">
+          Sort by
+        </label>
+        <select
+          className="filter_select"
+          aria-label="sort"
+          id="sort"
+          value={sorting.value}
+          onChange={handleChangeSorting}
+        >
+          <option value="relevance">relevance</option>
+          <option value="newest">newest</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default Filter;
