df66d12e0dc8b9964f8e4dad67c10da69930cf02
Anna Zhukova <anna.zhukanna@gmail.com>
2022-04-09 12:33:49 +0500

    feat: add redirect on home page after search submit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
-import Container from "react-bootstrap/Container";
+import React, { useState } from 'react';
+import { Routes, Route, Outlet, useNavigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
-import Header from "./components/Header.js";
-import Home from "./pages/home.js";
-import Book from "./pages/book.js";
-import context from "./Context.js";
+import Header from './components/Header.js';
+import Home from './pages/home.js';
+import Book from './pages/book.js';
+import context from './Context.js';
 
 export default function App() {
   return (
     <Routes>
-      <Route path={"/"} element={<Layout />}>
+      <Route path={'/'} element={<Layout />}>
         <Route index element={<Home />} />
         <Route path={`book/:id`} element={<Book />} />
       </Route>
@@ -19,24 +19,22 @@ export default function App() {
 }
 
 function Layout() {
-  const [search, setSearch] = useState("react");
-  const [value, setValue] = useState("");
+  const navigate = useNavigate();
+  const [search, setSearch] = useState('react');
+  const [value, setValue] = useState('');
 
   const onChangeValue = (e) => setValue(e.target.value);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     setSearch(value);
+    navigate('/');
   };
 
   return (
     <context.Provider value={search}>
       <Container>
-        <Header
-          onSubmit={handleFormSubmit}
-          onChangeValue={onChangeValue}
-          value={value}
-        />
+        <Header onSubmit={handleFormSubmit} onChangeValue={onChangeValue} value={value} />
         <Outlet />
       </Container>
     </context.Provider>
