0888c8e498b7298c21614c0475102517a81247f7
Anna Zhukova <anna.zhukanna@gmail.com>
2022-04-18 00:23:15 +0500

    feat: add 404 page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout.js';
 import Home from './pages/home.js';
 import Book from './pages/book.js';
+import Nopage from './pages/404.js';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path={`book/:id`} element={<Book />} />
       </Route>
+      <Route path={'*'} element={<Nopage />} />
     </Routes>
   );
 }
